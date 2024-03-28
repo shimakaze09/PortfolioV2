ef18addc5f8f98d3bc3d674d322742e58d95710d
John <yangtianzhuo970425@gmail.com>
2024-03-28 21:04:56 +1300

    Hamburger menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,63 @@
+"use client"
+
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+const links = [
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About" },
+    { url: "/portfolio", title: "Portfolio" },
+    { url: "/contact", title: "Contact" }
+]
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            {/* LINKS */}
+            <div className="hidden md:flex gap-4 w-1/3">
+                {links.map(link => (
+                    <Link href={link.url} key={link.title}>{link.title}</Link>
+                ))}
+            </div>
             {/* LOGO */}
-            <div className="">
+            <div className="md:hidden lg:flex w-1/3 justify-center">
                 <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                     <span className="text-white mr-1">J Yang</span>
-                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+                        .dev
+                    </span>
+                </Link>
+            </div>
+            {/* SOCIALS */}
+            <div className="hidden md:flex gap-4 w-1/3">
+                <Link href={"https://github.com/shimakaze09"}>
+                    <Image src="/github.png" alt="" width={24} height={24} />
+                </Link>
+                <Link href={"https://www.linkedin.com/in/john-yang-70a5722a6/"}>
+                    <Image src="/linkedin.png" alt="" width={24} height={24} />
                 </Link>
             </div>
             {/* RESPONSIVE MENU */}
-            <div>
+            <div className="md:hidden">
+                {/* MENU BUTTON */}
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(() => setOpen(!open))}>
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                </button>
+                {/* MENU LIST */}
+                {
+                    open && (
+                        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                            {links.map(link => (
+                                <Link href={link.url} key={link.title} className="">{link.title}</Link>
 
+                            ))}
+                        </div>
+                    )}
             </div>
         </div>
     )
