5876309d423e8ed4f1e32e0e0bea8603ff950e80
John <yangtianzhuo970425@gmail.com>
2024-03-28 23:00:09 +1300

    Homepage design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        {/* TODO: CHANGE TO THREE.JS */}
+        <Image src="/hero.png" alt="" fill className="object-contain" />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* TITLE */}
+        <h1 className="text-4xl md:text-6xl font-bold">Creative Craftsmanship in Code and Canvas</h1>
+        {/* DESC */}
+        <p className="">
+          Welcome to my portfolio, a space where I share my journey in the world of game development and 3D artistry. Here, you will find a collection of projects that reflect my exploration and learning in C/C++, Unity, Blender, and full-stack development.
+        </p>
+        {/* BUTTONS */}
+        <div className="w-full flex gap-4">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
+          <button className="p-4 rounded-lg ring-1 ring-black">Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
