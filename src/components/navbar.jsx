24ec8118e7888d9a77042d4e47142e1331bb1d5e
John <yangtianzhuo970425@gmail.com>
2024-03-29 11:05:25 +1300

    Hamburger menu animation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,67 +4,116 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import NavLink from "./navLink";
+import { motion } from "framer-motion";
 
 const links = [
-    { url: "/", title: "Home" },
-    { url: "/about", title: "About" },
-    { url: "/portfolio", title: "Portfolio" },
-    { url: "/contact", title: "Contact" }
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" }
 ]
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    return (
-        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
-            {/* LINKS */}
-            <div className="hidden md:flex gap-4 w-1/3">
-                {links.map(link => (
-                    <NavLink link={link} key={link.title} />
-                ))}
-            </div>
-            {/* LOGO */}
-            <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
-                <Link
-                    href="/"
-                    className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
-                >
-                    <span className="text-white mr-1">J Yang</span>
-                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
-                        .dev
-                    </span>
-                </Link>
-            </div>
-            {/* SOCIALS */}
-            <div className="hidden md:flex gap-4 w-1/3">
-                <Link href={"https://github.com/shimakaze09"}>
-                    <Image src="/github.png" alt="" width={24} height={24} />
-                </Link>
-                <Link href={"https://www.linkedin.com/in/john-yang-70a5722a6/"}>
-                    <Image src="/linkedin.png" alt="" width={24} height={24} />
-                </Link>
-            </div>
-            {/* RESPONSIVE MENU */}
-            <div className="md:hidden">
-                {/* MENU BUTTON */}
-                <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(() => setOpen(!open))}>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                </button>
-                {/* MENU LIST */}
-                {
-                    open && (
-                        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-                            {links.map(link => (
-                                <Link href={link.url} key={link.title} className="">{link.title}</Link>
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)"
+    }
+  }
 
-                            ))}
-                        </div>
-                    )}
-            </div>
-        </div>
-    )
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    }
+  }
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)"
+    }
+  }
+
+  const listVariants = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: 0,
+    }
+  }
+
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map(link => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
+      {/* LOGO */}
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">J Yang</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
+        </Link>
+      </div>
+      {/* SOCIALS */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href={"https://github.com/shimakaze09"}>
+          <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href={"https://www.linkedin.com/in/john-yang-70a5722a6/"}>
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
+      {/* RESPONSIVE MENU */}
+      <div className="md:hidden">
+        {/* MENU BUTTON */}
+        <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(() => setOpen(!open))}>
+          <motion.div
+            variants={topVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
+        </button>
+        {/* MENU LIST */}
+        {open && (
+          <motion.div variants={listVariants} initial="closed" animate="opened" className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map(link => (
+              <Link href={link.url} key={link.title} className="">{link.title}</Link>
+            ))}
+          </motion.div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Navbar;
