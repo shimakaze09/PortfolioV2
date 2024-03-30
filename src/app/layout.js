91d132e78f7618f6ef4d938bbfb14a58c12b8dae
John <yangtianzhuo970425@gmail.com>
2024-03-30 23:11:21 +1300

    Fixing bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +16,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
